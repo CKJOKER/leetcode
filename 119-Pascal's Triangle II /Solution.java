621b5fe0da17d5c2fdc7cf3d1199814692103fcc
pig <ckjoker78@gmail.com>
2015-02-24 23:04:17 +0800

    --

--- 119-Pascal's Triangle II /Solution.java
+++ 119-Pascal's Triangle II /Solution.java
@@ -17,7 +17,7 @@ public class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> row=new ArrayList<Integer>();
         row.add(1);
-        for(int i=0;i<rowIndex;++i){
+        for(int i=0;i<rowIndex;++i){       //若k=3代表第3行，则两个for循环改为i=1，j=i-1即可
            for(int j=i;j>0;--j){
            	   row.set(j,row.get(j-1)+row.get(j));  //计算新元素要用到前一行产生的元素，所以要从后往前计算，避免新一行的元素刚产生又被直接哪来计算（后面的元素会用到前面的，但是前面的不会用到后面的）
             }
